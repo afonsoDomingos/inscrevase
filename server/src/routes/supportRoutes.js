05ca72900b0fc5beafea61a21d5de24102b15812
afonsoDomingos <karinganastudio23@gmail.com>
2026-01-07 12:41:34 +0200

    fix: Correct middleware names in supportRoutes (authMiddleware/adminMiddleware)

--- server/src/routes/supportRoutes.js
+++ server/src/routes/supportRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { protect, authorize } = require('../middleware/authMiddleware');
+const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
 const { createTicket, getMyTickets, getAllTickets, addMessage } = require('../controllers/supportController');
 
-router.post('/', protect, createTicket);
-router.get('/my', protect, getMyTickets);
-router.get('/all', protect, authorize('admin'), getAllTickets);
-router.post('/:id/message', protect, addMessage);
+router.post('/', authMiddleware, createTicket);
+router.get('/my', authMiddleware, getMyTickets);
+router.get('/all', authMiddleware, adminMiddleware, getAllTickets);
+router.post('/:id/message', authMiddleware, addMessage);
 
 module.exports = router;
