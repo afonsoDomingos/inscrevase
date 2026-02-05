66930693120501ba16134089b93945ab473c97d9
afonsoDomingos <karinganastudio23@gmail.com>
2026-02-05 18:34:48 +0200

    Optimize robots.txt and robots.ts for AdSense crawler compliance

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/dashboard/', '/admin/', '/api/', '/private/'],
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: ['/', '/_next/static/'],
+                disallow: ['/dashboard/', '/admin/', '/api/', '/private/'],
+            },
+            {
+                userAgent: 'Mediapartners-Google',
+                allow: '/',
+            }
+        ],
         sitemap: 'https://inscreva-se.com/sitemap.xml',
     };
 }
