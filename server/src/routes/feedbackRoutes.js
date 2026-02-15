67f7f152342df712656805ae5aa60465923fe131
afonsoDomingos <karinganastudio23@gmail.com>
2026-02-15 22:43:20 +0200

    fix: feedback routes middleware import and admin protection

--- server/src/routes/feedbackRoutes.js
+++ server/src/routes/feedbackRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/feedbackController');
-const auth = require('../middleware/authMiddleware');
+const { authMiddleware: auth, adminMiddleware } = require('../middleware/authMiddleware');
 
 // Public route to send feedback (userId optional in controller)
 router.post('/', async (req, res, next) => {
@@ -15,7 +15,7 @@ router.post('/', async (req, res, next) => {
 
 // Protected routes
 router.get('/my', auth, feedbackController.getFeedbacksForUser);
-router.get('/admin/all', auth, feedbackController.getAllFeedbacksAdmin);
-router.patch('/:id/status', auth, feedbackController.updateFeedbackStatus);
+router.get('/admin/all', auth, adminMiddleware, feedbackController.getAllFeedbacksAdmin);
+router.patch('/:id/status', auth, adminMiddleware, feedbackController.updateFeedbackStatus);
 
 module.exports = router;
