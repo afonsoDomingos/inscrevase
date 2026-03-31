9aa8f96c4b9cde790dae542999f032cee709fcaa
afonsoDomingos <karinganastudio23@gmail.com>
2026-03-31 17:13:03 +0200

    fix: ensure meta pixel tracks pageview on initial load

--- client/src/components/MetaPixel.tsx
+++ client/src/components/MetaPixel.tsx
@@ -36,6 +36,7 @@ export default function MetaPixel({ pixelId }: Props) {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
           `,
                 }}
             />
